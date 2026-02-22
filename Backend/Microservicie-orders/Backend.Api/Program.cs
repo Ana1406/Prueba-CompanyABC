528f8862b9c98bfeb38ed06b161ed2b3eceda2d8
ana1406 <anamariar317@gmail.com>
2026-02-22 11:45:37 -0500

    docker compose and allows swagger

--- Backend/Microservicie-orders/Backend.Api/Program.cs
+++ Backend/Microservicie-orders/Backend.Api/Program.cs
@@ -28,12 +28,9 @@ builder.Services.AddSwaggerGen(c =>
 });
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 app.UseRouting();
